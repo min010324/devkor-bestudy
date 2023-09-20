d5a6e79aaf8054ae97a21049f28a92b59c3b10ff
jeongmin hwang <jeongmin010324@gmail.com>
2023-09-20 19:16:08 +0900

    feat : user 정보 get, insert, delete 작업

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { UserDto } from './dto/user.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppService.name);
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/users/:id")
+  getUserNameById(@Param('id') id: number, @Res() res: Response) {
+    try {
+      res.json(this.appService.getUser(id));
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  @Get("/users")
+  getUserNameList(@Res() res: Response) {
+    try {
+      res.json(this.appService.getUserList());
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  @Post("/users")
+  insertUser(@Body('user') createUser: UserDto, @Res() res: Response) {
+    try {
+      this.logger.log(`create user name : ${createUser.userName}`);
+      res.json(this.appService.insertUser(createUser));
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  @Delete("/users/:id")
+  deleteUser(@Param("id") id: number, @Res() res: Response) {
+    try {
+      res.json(this.appService.deleteUser(id));
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+  // https://velog.io/@ehgks0000/HTTP-GET-DELETE-With-Body
+
 }
